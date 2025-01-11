c20488f51a6f8a459296009103626182bfdd91a0
Rachum-thu <369933193@qq.com>
2025-01-11 16:52:56 -0600

    Add initial test suite for chat functionality and update .gitignore

---
+++ test/test_chat.py
@@ -0,0 +1,6 @@
+from toolkit.openai.chat import chat
+
+results = chat([f'Who is considered the most famous NBA basketball player with number {i}' for i in range(0, 50)])
+for idx, res in enumerate(results):
+    print(f'Q: Who is considered the most famous NBA basketball player with number {idx}?')
+    print(f'A: {res}'.replace('\n', ' ') + '\n')
